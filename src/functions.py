4fe4bd0aaba86914fb126823e1ed1c771fd34c92
MountainWhale <spacemarine724@gmail.com>
2020-12-10 10:34:48 +1000

    actually commit functions.py

---
+++ src/functions.py
@@ -0,0 +1,10 @@
+def get_path():
+    # Specify the path to your warp.db here e.g /home/cheese/warp/warp.db
+    path = "/cheese"
+    return path
+
+
+def get_shell():
+    # Specify which shell you're using bash/zsh or other
+    shell = "cheese"
+    return shell
